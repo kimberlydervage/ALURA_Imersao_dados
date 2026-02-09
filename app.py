7e535468463503d67221e4fd525be80e56b45dd6
kimberlydervage <kim.dervage@gmail.com>
2026-02-09 17:36:15 -0300

    Teste mínimo

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
 import streamlit as st
 
-st.set_page_config(page_title="Teste", layout="wide")
-st.title("🚀 O App funcionou!")
-st.write("Se você está lendo isso, o ambiente está perfeito.")
+st.title("Teste Funcionou!")
